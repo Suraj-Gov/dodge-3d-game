eba1172185e3b428e1eeb708ff0d516c9e981325
Suraj Govind <surajgovind332000@gmail.com>
2019-08-18 23:26:49 +0530

    Added gameover and it's functionalities.
    Game is 60% complete.

--- Assets/Scenes/Scripts/gameState.cs
+++ Assets/Scenes/Scripts/gameState.cs
@@ -7,6 +7,7 @@ public class gameState : MonoBehaviour
 {
     public float scaleSlowmo = 10f;
     public float slowmoDuration = 10f;
+    public GameObject endgameUI;
     
     
 
@@ -18,40 +19,51 @@ public class gameState : MonoBehaviour
         //this is called when player hits an obstacle
     }
 
-    
 
-    
 
-  
+
+
+
 
     IEnumerator RestartLevel()
     {
-        
+
 
 
         Time.timeScale = 1f / scaleSlowmo;      //slows time by dividing the scale with slowmo value
                                                 //like 1/2, where 2 is the scale, time slows down to 0.5 from 1.0
         Time.fixedDeltaTime = Time.fixedDeltaTime / scaleSlowmo; //timescale is 1 by default
                                                                  //we use fixed deltatime, that is different from time.time, which we don't use, but it's run every time.
+
         yield return new WaitForSeconds(slowmoDuration / scaleSlowmo);
+        endgameUI.SetActive(true);
+
+        
+
+
 
         
         
-            
-            Time.timeScale = Time.timeScale * scaleSlowmo;
-            Time.fixedDeltaTime = Time.fixedDeltaTime * scaleSlowmo;
-            //used to reset time, by dividing to remove the multiplier
-
-
-            
-            SceneManager.LoadScene("MainLevel01");
-        //waits for 10 sec and then passes control to below code
-
-
-        //loads the main level
     }
 
-    
+    public void retryGame()
+    {
+        Time.timeScale = Time.timeScale * scaleSlowmo;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * scaleSlowmo;
+        //used to reset time, by dividing to remove the multiplier
+        
 
-    
+
+        SceneManager.LoadScene("MainLevel01");
+    }
+
+    public void endApp()
+    {
+        Time.timeScale = Time.timeScale * scaleSlowmo;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * scaleSlowmo;
+
+        SceneManager.LoadScene(SceneManager.sceneCount - 1);
+    }
+
+
 }
